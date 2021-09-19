e0823fa2a9c8406db9524de157f442ef29b08a80
Anita Suoliman <87454390+ReenaSuoliman@users.noreply.github.com>
2021-09-19 22:06:09 +0200

    Initialize aeschli/parameterized-textdocuments

---
+++ client-tests/src/servers/testInitializeResult.ts
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+import * as assert from 'assert';
+import {
+	createConnection, IConnection,
+	TextDocuments, InitializeParams, ServerCapabilities, CompletionItemKind, ResourceOperationKind, FailureHandlingKind, DiagnosticTag, CompletionItemTag, TextDocumentSyncKind
+} from 'vscode-languageserver/lib/main';
+
+let connection: IConnection = createConnection();
+
+console.log = connection.console.log.bind(connection.console);
+console.error = connection.console.error.bind(connection.console);
+
+let documents: TextDocuments = TextDocuments.create();
+documents.listen(connection);
+
+connection.onInitialize((params: InitializeParams): any => {
+	assert.equal((params.capabilities.workspace as any).applyEdit, true);
+	assert.equal(params.capabilities.workspace!.workspaceEdit!.documentChanges, true);
+	assert.deepEqual(params.capabilities.workspace!.workspaceEdit!.resourceOperations, [ResourceOperationKind.Create, ResourceOperationKind.Rename, ResourceOperationKind.Delete]);
+	assert.equal(params.capabilities.workspace!.workspaceEdit!.failureHandling, FailureHandlingKind.TextOnlyTransactional);
+	assert.equal(params.capabilities.textDocument!.completion!.completionItem!.deprecatedSupport, true);
+	assert.equal(params.capabilities.textDocument!.completion!.completionItem!.preselectSupport, true);
+	assert.equal(params.capabilities.textDocument!.completion!.completionItem!.tagSupport!.valueSet.length, 1);
+	assert.equal(params.capabilities.textDocument!.completion!.completionItem!.tagSupport!.valueSet[0], CompletionItemTag.Deprecated);
+	assert.equal(params.capabilities.textDocument!.signatureHelp!.signatureInformation!.parameterInformation!.labelOffsetSupport, true);
+	assert.equal(params.capabilities.textDocument!.definition!.linkSupport, true);
+	assert.equal(params.capabilities.textDocument!.declaration!.linkSupport, true);
+	assert.equal(params.capabilities.textDocument!.implementation!.linkSupport, true);
+	assert.equal(params.capabilities.textDocument!.typeDefinition!.linkSupport, true);
+	assert.equal(params.capabilities.textDocument!.rename!.prepareSupport, true);
+	assert.equal(params.capabilities.textDocument!.publishDiagnostics!.relatedInformation, true);
+	assert.equal(params.capabilities.textDocument!.publishDiagnostics!.tagSupport!.valueSet.length, 2);
+	assert.equal(params.capabilities.textDocument!.publishDiagnostics!.tagSupport!.valueSet[0], DiagnosticTag.Unnecessary);
+	assert.equal(params.capabilities.textDocument!.publishDiagnostics!.tagSupport!.valueSet[1], DiagnosticTag.Deprecated);
+	assert.equal(params.capabilities.textDocument!.documentLink!.tooltipSupport, true);
+	let valueSet = params.capabilities.textDocument!.completion!.completionItemKind!.valueSet!;
+	assert.equal(valueSet[0], 1);
+	assert.equal(valueSet[valueSet.length - 1], CompletionItemKind.TypeParameter);
+	console.log(params.capabilities);
+
+	let capabilities: ServerCapabilities = {
+		textDocumentSync: TextDocumentSyncKind.Full,
+		completionProvider: { resolveProvider: true, triggerCharacters: ['"', ':'] },
+		hoverProvider: true,
+		renameProvider: {
+			prepareProvider: true
+		}
+	};
+	return { capabilities, customResults: { hello: 'world' } };
+});
+
+connection.onInitialized(() => {
+	connection.sendDiagnostics({ uri: 'uri:/test.ts', diagnostics: [] });
+});
+
+// Listen on the connection
+connection.listen();
---
+++ protocol/src/protocol.colorProvider.ts
@@ -0,0 +1,85 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import { RequestType, RequestHandler, ProgressType } from 'vscode-jsonrpc';
+import { TextDocumentRegistrationOptions, StaticRegistrationOptions, PartialResultParams, WorkDoneProgressParams, WorkDoneProgressOptions } from './protocol';
+import { TextDocumentIdentifier, Range, Color, ColorInformation, ColorPresentation } from 'vscode-languageserver-types';
+
+//---- Client capability ----
+
+export interface DocumentColorClientCapabilities {
+	/**
+	 * Whether implementation supports dynamic registration. If this is set to `true`
+	 * the client supports the new `DocumentColorRegistrationOptions` return value
+	 * for the corresponding server capability as well.
+	 */
+	dynamicRegistration?: boolean;
+}
+
+export interface DocumentColorOptions extends WorkDoneProgressOptions {
+	/**
+	 * Code lens has a resolve provider as well.
+	 */
+	resolveProvider?: boolean;
+}
+
+export interface DocumentColorRegistrationOptions extends TextDocumentRegistrationOptions, StaticRegistrationOptions, DocumentColorOptions {
+}
+
+//---- Color Symbol Provider ---------------------------
+
+/**
+ * Parameters for a [DocumentColorRequest](#DocumentColorRequest).
+ */
+export interface DocumentColorParams extends WorkDoneProgressParams, PartialResultParams {
+	/**
+	 * The text document.
+	 */
+	textDocument: TextDocumentIdentifier;
+}
+
+/**
+ * A request to list all color symbols found in a given text document. The request's
+ * parameter is of type [DocumentColorParams](#DocumentColorParams) the
+ * response is of type [ColorInformation[]](#ColorInformation) or a Thenable
+ * that resolves to such.
+ */
+export namespace DocumentColorRequest {
+	export const type = new RequestType<DocumentColorParams, ColorInformation[], void, DocumentColorRegistrationOptions>('textDocument/documentColor');
+	export const resultType = new ProgressType<ColorInformation[]>();
+	export type HandlerSignature = RequestHandler<DocumentColorParams, ColorInformation[], void>;
+}
+
+/**
+ * Parameters for a [ColorPresentationRequest](#ColorPresentationRequest).
+ */
+export interface ColorPresentationParams extends WorkDoneProgressParams, PartialResultParams {
+	/**
+	 * The text document.
+	 */
+	textDocument: TextDocumentIdentifier;
+
+	/**
+	 * The color to request presentations for.
+	 */
+	color: Color
+
+	/**
+	 * The range where the color would be inserted. Serves as a context.
+	 */
+	range: Range;
+}
+
+/**
+ * A request to list all presentation for a color. The request's
+ * parameter is of type [ColorPresentationParams](#ColorPresentationParams) the
+ * response is of type [ColorInformation[]](#ColorInformation) or a Thenable
+ * that resolves to such.
+ */
+export namespace ColorPresentationRequest {
+	export const type = new RequestType<ColorPresentationParams, ColorPresentation[], void, WorkDoneProgressOptions & TextDocumentRegistrationOptions>('textDocument/colorPresentation');
+	export type HandlerSignature = RequestHandler<ColorPresentationParams, ColorPresentation[], void>;
+}
